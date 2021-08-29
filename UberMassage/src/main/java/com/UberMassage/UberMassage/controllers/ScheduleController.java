ae4c73765dea499e0e17cb1b600fbfe8f13698b5
Lancaster Graham <Lancastergraham87@gmail.com>
2021-08-29 14:52:56 -0500

    Bit of a logic hurdle, but a User can now logout, and also can not schedule themselves for an appointment.

--- UberMassage/src/main/java/com/UberMassage/UberMassage/controllers/ScheduleController.java
+++ UberMassage/src/main/java/com/UberMassage/UberMassage/controllers/ScheduleController.java
@@ -52,7 +52,7 @@ public class ScheduleController {
         User theUser = getUserFromSession(request.getSession());
 
         model.addAttribute("title", "This is schedule");
-        model.addAttribute("user", theUser);
+        model.addAttribute("client", theUser);
         model.addAttribute("users", userRepository.findAll());
 
 
@@ -66,6 +66,7 @@ public class ScheduleController {
         User theUser = getUserFromSession(request.getSession());
         model.addAttribute("title", "This is schedule");
         model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("client", theUser);
 
         User therapist = userRepository.findById(therapistId).orElse(new User());
 
